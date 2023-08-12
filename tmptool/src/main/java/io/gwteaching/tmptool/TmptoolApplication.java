b48fe45dee923800f5242f87d10b599b8e5d3634
GW0602Teaching <gw0602.teaching@gmail.com>
2023-08-12 11:51:18 -0700

    Init phase-2

---
+++ tmptool/src/main/java/io/gwteaching/tmptool/TmptoolApplication.java
@@ -0,0 +1,13 @@
+package io.gwteaching.tmptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmptoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmptoolApplication.class, args);
+	}
+
+}
---
+++ tmptool/src/test/java/io/gwteaching/tmptool/TmptoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gwteaching.tmptool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmptoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
