063fc213459bace321f3da7b3d6c6dacb30217ab
GW0602Teaching <gw0602.teaching@gmail.com>
2023-08-12 22:53:16 -0700

    Object Validation p1

--- tmptool/src/main/java/io/gwteaching/tmptool/dto/Project.java
+++ tmptool/src/main/java/io/gwteaching/tmptool/dto/Project.java
@@ -1,10 +1,13 @@
 package io.gwteaching.tmptool.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -16,18 +19,27 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project Name is required")
     private String projectName;
 
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 6, message = "Please use 4 to 6 characters")
+    @Column(updatable = false, unique = true)
     private String projectId;
 
+    @NotBlank(message = "Project description is required")
     private String description;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createAt;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updateAt;
 
     @PrePersist
