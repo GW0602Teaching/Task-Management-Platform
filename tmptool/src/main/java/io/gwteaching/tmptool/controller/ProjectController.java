91897793839d2a77682b2706c63b83b6ffdbadf2
GW0602Teaching <gw0602.teaching@gmail.com>
2023-08-21 22:10:37 -0700

    Add get method

--- tmptool/src/main/java/io/gwteaching/tmptool/controller/ProjectController.java
+++ tmptool/src/main/java/io/gwteaching/tmptool/controller/ProjectController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -38,6 +35,12 @@ public class ProjectController {
 
         Project newProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(newProject, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{projectId}")
+    public ResponseEntity<?> getProjectByProjectId(@PathVariable String projectId) {
+        Project project = projectService.findProjectById(projectId);
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
 
 }
--- tmptool/src/main/java/io/gwteaching/tmptool/services/ProjectService.java
+++ tmptool/src/main/java/io/gwteaching/tmptool/services/ProjectService.java
@@ -22,4 +22,8 @@ public class ProjectService {
 
     }
 
+    public Project findProjectById(String projectId) {
+        return projectRepository.findByProjectId((projectId));
+    }
+
 }
