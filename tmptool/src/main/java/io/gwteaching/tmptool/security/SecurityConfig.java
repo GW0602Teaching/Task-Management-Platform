5584729a687918c0e7acdabaa932bac3b30dc749
GW0602Teaching <gw0602.teaching@gmail.com>
2023-09-05 23:16:58 -0700

    Refactor Security config file to add Auth Manager

--- tmptool/src/main/java/io/gwteaching/tmptool/security/SecurityConfig.java
+++ tmptool/src/main/java/io/gwteaching/tmptool/security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package io.gwteaching.tmptool.security;
 
+import io.gwteaching.tmptool.services.UserCustomizedDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,13 +26,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
         jsr250Enabled = true,
         prePostEnabled = true
 )
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Autowired
+    private UserCustomizedDetailsService userCustomizedDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -36,8 +47,31 @@ public class SecurityConfig {
                 .antMatchers(SIGN_UP_URLS).permitAll()
                 .antMatchers(H2_ADD).permitAll()
                 .anyRequest().authenticated();
-        return http.build();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+       authenticationManagerBuilder.userDetailsService(userCustomizedDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    //    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable()
+//                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+//                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and().headers().frameOptions().sameOrigin() // To enable H2 database
+//                .and().authorizeRequests().antMatchers("/", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg", "/**/*.html", "/**/*.css", "/**/*.js").permitAll()
+//                .antMatchers(SIGN_UP_URLS).permitAll()
+//                .antMatchers(H2_ADD).permitAll()
+//                .anyRequest().authenticated();
+//        return http.build();
+//    }
 
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder() {
